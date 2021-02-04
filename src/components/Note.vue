7f347f39dc5b5c98450d67d3ad83b549d43e8e20
98claes98 <claes.and98@hotmail.com>
2021-02-04 11:13:18 +0100

    Made note text non selectable

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -2,19 +2,19 @@
   <div class="container-fluid" id="noteBox">
     <div class="row text-center py-3">
       <div class="col">
-        <h2>
+        <h2 class="noselect">
           <u>{{ title }}</u>
         </h2>
       </div>
     </div>
     <div v-if="description" class="row text-center px-4">
       <div class="col">
-        <p>{{ description }}</p>
+        <p class="noselect">{{ description }}</p>
       </div>
     </div>
     <div v-if="date" class="row text-center">
       <div class="col">
-        <p id="date">
+        <p class="noselect" id="date">
           {{ localDate }}
         </p>
       </div>
@@ -43,5 +43,15 @@ export default {
 #noteBox {
   background-color: rgb(255, 255, 67);
   box-shadow: 5px 5px 7px rgba(33, 33, 33, 0.7);
+}
+
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
 }
 </style>
